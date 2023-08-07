b5f1464ab0d8a8de449e5e39f78dd12d881d4e58
Ben <55010051+bendejong0@users.noreply.github.com>
2023-08-07 11:43:38 -0500

    8-7-2023 Updates / DevLog
    - Added a printsong function so users can see the songs in their playlist
    - Allowed the ability users to add, delete, or print songs in their playlist. (Menu::SelectPlaylist())
    - Fixed some bugs
    - Learned more about cin.ignore()
    - Made it so that all songs and artist names get stored in all caps so that you can find them easier and they are not case sensitive.

--- main.cpp
+++ main.cpp
@@ -2,8 +2,7 @@
 #include <string>
 #include <iomanip>
 #include <unordered_map>
-
-
+#include <algorithm>
 using namespace std;
 
 class Node{
@@ -11,8 +10,6 @@ private:
     string _title;
     string _artist;
     long _songId;
-    
-    
     
 public:
     Node(string a = "", string b = "", long c= 0, Node* d = nullptr){
@@ -33,19 +30,19 @@ public:
     }
     void PrintSong(Node* current){
         while(current -> _next != nullptr){
-            cout << current -> _title << endl;
+            cout << "Song title: " << current -> _title << ", Artist name: " <<  current -> _artist << ", Song ID: " << _songId << endl;
             current = current -> _next;
         }
-        cout << current -> _title << endl;
+        cout << "Song title: " << current -> _title << ", Artist name: " <<  current -> _artist << ", Song ID: " << _songId << endl;
     }
 
     void DeleteSong(Node* current, string songTitle = "", string artistName = "", long songID = 0){
         Node* previous = current;
-        while(current -> _title != songTitle || current  -> _artist != artistName ||  current  -> _songId != songID){
+        while( ( current -> _title != songTitle && current  -> _artist != artistName ) ||  current  -> _songId != songID){
             previous = current;
             current = current -> _next;
         }
-        if (current -> _title == songTitle || current -> _artist == artistName ||  current -> _songId == songID){
+        if ((current -> _title == songTitle && current -> _artist == artistName) ||  current -> _songId == songID){
             Node* deleteThisNode = current;
             current = current -> _next;
             delete deleteThisNode;
@@ -146,29 +143,37 @@ public:
         int user;
         cout << "Select what playlist you'd like to listen to: ";
         cin >> user;
+        cin.ignore();
         playlistMap.find(user);
         cout << endl << "Opening..."  << endl << endl;
-
+        
         
         enum songOptions{
             ADD = 1,
             DELETE = 2,
+            PRINT = 3,
             EXIT = 4
         };
+        
         songOptions songOption;
-        // this is used to select what the user wants to do to the songs in their playlist.
+        //this is used to select what the user wants to do to the songs in their playlist.
         int songSelectUser;
+        
+        
+        string songTitle;
+        string artistName;
+        long songID;
+
         if(current-> _firstSongAddress == nullptr){
             int addOrGoBack;
             cout << "This playlist is empty!" << endl;
             cout << " 1) Add a song\n 2) Go back" << endl;
             cout << "Input your option's corresponding number: ";
             cin >> addOrGoBack;
+            cin.ignore();
 
 
             if(addOrGoBack == 1){
-                string songTitle;
-                string artistName;
                 long songID;
                 cout << "Enter the song's title: ";
                 getline(cin, songTitle);
@@ -178,6 +183,9 @@ public:
                 cin.ignore();
                 cout << "Enter the song's ID: ";
                 cin >> songID;
+                cin.ignore();
+                transform(songTitle.begin(), songTitle.end(), songTitle.begin(), ::toupper);
+                transform(artistName.begin(), artistName.end(), artistName.begin(), ::toupper);
                 Node* temp = new Node(songTitle, artistName, songID);
                 current -> _firstSongAddress = temp;
             }
@@ -189,7 +197,68 @@ public:
             }
         }
         
-        else if(current-> _firstSongAddress!=nullptr){
+        else if(current -> _firstSongAddress != nullptr){
+            Node* firstSong = current -> _firstSongAddress;
+
+            cout << "What would you like to do?\n 1) Add a song\n 2) Delete a song\n 3) Show songs\n 4) Go back" << endl;
+            cin >> songSelectUser;
+            cin.ignore();
+            songOption = static_cast<songOptions>(songSelectUser);
+
+            // we will need this data for cases ADD and DELETE.
+            string songTitle;
+            string artistName;
+            long songID;
+
+            // we need this node so we can access the addsong and deletesong functions.
+            Node node;
+
+            switch(songOption){
+                case ADD:
+                    cout << "Enter the song's title: ";
+                    getline(cin, songTitle);
+                    cin.ignore();
+                    cout << "Enter the artist's name: ";
+                    getline(cin, artistName);
+                    cin.ignore();
+                    cout << "Enter the song's ID: ";
+                    cin >> songID;
+                    node.AddSong(firstSong, songTitle, artistName, songID);
+
+                    break;
+
+                case DELETE:
+                    
+                    cout << "We need some info! Please tell us what you rememeber.\nEnter 0 if you don't remember the Song's ID number" << endl;
+                    cout << "Enter the song's title: ";
+                    getline(cin, songTitle);
+                    cin.ignore();
+                    cout << "Enter the artist's name: ";
+                    getline(cin, artistName);
+                    cin.ignore();
+                    cout << "Enter the song's ID: ";
+                    cin >> songID;
+                    cin.ignore();
+
+                    // convert everything to uppercase.
+                    transform(songTitle.begin(), songTitle.end(), songTitle.begin(), ::toupper);
+                    transform(artistName.begin(), artistName.end(), artistName.begin(), ::toupper);
+                    if(songID == 0){
+                        node.DeleteSong(firstSong, songTitle, artistName);
+                    }
+                    else{
+                        node.DeleteSong(firstSong, songTitle, artistName, songID);
+                    }
+                    break;
+
+                case PRINT:
+                    node.PrintSong(firstSong);
+                    break;
+            }
+
+        }
+
+        /*else if(current-> _firstSongAddress!=nullptr){
             // the firstSong node basically functions as the head node.
             Node* firstSong = current -> _firstSongAddress;
             Node* temp = firstSong;
@@ -219,7 +288,7 @@ public:
 
                 }
             }
-        }
+        }*/
     }
 
     // function that processes user input
@@ -227,6 +296,7 @@ public:
         string plName;
         int user;
         cin >> user;
+        cin.ignore();
         options optionlist = static_cast<options>(user);
         Tree tree;
 
@@ -235,8 +305,8 @@ public:
 
             case CREATE:
                 cout << "Name your playlist: ";
-                cin.ignore();
                 getline(cin, plName);
+                cin.ignore();
                 while (head->_next != nullptr) {
                     head = head->_next;
                 }
@@ -295,12 +365,12 @@ int main()
     string plName;
     int user;
     cin >> user;
+    cin.ignore();
 
     Tree* head;
         
     if (user == 1) {
         cout << "Name your playlist: ";
-        cin.ignore(); // Clear the newline character from the buffer
         getline(cin, plName);
         head = new Tree(plName); // Use the existing 'head' pointer
     } 
@@ -309,6 +379,7 @@ int main()
             cout << "Please choose a valid option!" << endl;
             cout << "Press 1 to create your first playlist" << endl;
             cin >> user;
+            cin.ignore();
             if (user == 1) {
                 break;
             }
